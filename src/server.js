b0b3ff3f92cf1c94cffaea4cddff87220e91d6bb
douglas martins santos <douglas.santos@avecbrasil.com.br>
2019-08-07 16:34:54 -0300

    update 2

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
 
+
+mongoose.connect('mongodb+srv://dougprogrammer:EisMeAqui@&741@cluster0-l6stj.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
